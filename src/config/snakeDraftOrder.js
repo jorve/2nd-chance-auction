b2f410fb6f6b9df90f77525457e935359c3cea34
Jeff Jorve <jjorve@gmail.com>
2026-03-21 22:58:59 -0400

    changed keeper behavior

--- src/config/snakeDraftOrder.js
+++ src/config/snakeDraftOrder.js
@@ -13,17 +13,17 @@ export const MY_TEAM_ABBR = 'BEDST'
  * Round-1 snake order = array order.
  */
 export const SNAKE_DRAFT_ORDER = [
-  { abbr: 'SAVG2', name: '2025 Savages 2', owner: 'Neil', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
-  { abbr: 'JAYHK', name: 'Jayhawks', owner: 'Jay W.', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
-  { abbr: 'INGBT', name: 'Inglorious Batters', owner: 'Matt', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
   { abbr: 'HIGH2', name: 'Highlanders II', owner: 'Dennis', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
-  { abbr: 'CL21B', name: '21 Club 2', owner: 'Martin', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
-  { abbr: 'RUBYR', name: 'The Ruby Ring Gang A', owner: 'Norm', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
-  { abbr: 'MENDL', name: 'Mendel Lapse', owner: 'Arnie M', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
+  { abbr: 'MENDL', name: 'Mendel Lapse', owner: 'Arnie', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
   { abbr: 'BEDST', name: 'Bed Stuy Fish Fry', owner: 'Jeff J. (me)', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
+  { abbr: 'CL21B', name: '21 Club 2', owner: 'Marty', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
   { abbr: 'CASEY', name: 'Casey HotDog', owner: 'Toby', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
+  { abbr: 'SAVG2', name: '2025 Savages 2', owner: 'Neil', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
+  { abbr: 'JAYHK', name: 'Jayhawks', owner: 'Jay', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
   { abbr: 'CUCKOO', name: "Flew Over the Cuckoo's Nest", owner: 'John C.', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
+  { abbr: 'RUBYR', name: 'The Ruby Ring Gang A', owner: 'Norm', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
   { abbr: 'BEHEM', name: 'Behemoth', owner: 'Jon L.', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
+  { abbr: 'INGBT', name: 'Inglorious Batters', owner: 'Matt B.', slots_hit: 13, slots_pit: 9, slots: 38, budget: 300 },
 ]
 
 export const SNAKE_TEAM_ORDER = SNAKE_DRAFT_ORDER.map((r) => r.abbr)
